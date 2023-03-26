dfeae68efa2d49f295106f70c5c9f5793bd9c892
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-26 13:40:15 -0400

    leetcode self practices added

--- src/MyHashMap/MyHashMapApp.java
+++ src/MyHashMap/MyHashMapApp.java
@@ -8,14 +8,18 @@ public class MyHashMapApp {
         map.put(2, 20);
         map.put(3, 30);
 
-        map.printNodes(); // (1,10)(2,20)(3,30)
+        map.printNodes(); // {1=10} {2=20} {3=30}
         int param = map.get(3);
         System.out.println(param); // 3
         int param2 = map.get(30);
         System.out.println(param2); // -1
         map.put(4, 40);
-        map.printNodes(); // (1,10)(2,20)(3,30)(4,40)
+        map.printNodes(); // {1=10} {2=20} {3=30} {4=40}
         map.remove(3);
-        map.printNodes(); // (1,10)(2,20)(4,40)
+        map.printNodes(); // {1=10} {2=20} {4=40}
+        map.remove(1);
+        map.printNodes(); // {2=20} {4=40}
+        map.remove(4);
+        map.printNodes(); // {2=20}
     }
 }
