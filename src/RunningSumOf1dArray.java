8faf9646880fd17d803d2323b56dec561f9cccfe
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-06 12:27:22 -0400

    leetcode self practices added

--- src/RunningSumOf1dArray.java
+++ src/RunningSumOf1dArray.java
@@ -16,14 +16,10 @@ public class RunningSumOf1dArray {
 
     public static int[] runningSum(int[] nums) {
 
-        int[] newArray = new int[nums.length];
-
-        newArray[0] = nums[0];
-
         for (int i = 1; i < nums.length; i++) {
-            newArray[i] = newArray[i - 1] + nums[i];
+            nums[i] = nums[i - 1] + nums[i];
         }
-        return newArray;
+        return nums;
     }
 }
 /*
