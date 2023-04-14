a400d8c110c080d6fc93cad37c89e5714e3b6800
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-14 13:24:35 -0400

    leetcode self practices added

--- src/easy/RemoveDuplicatesFromSortedList/Solution.java
+++ src/easy/RemoveDuplicatesFromSortedList/Solution.java
@@ -19,3 +19,7 @@ public class Solution {
         return head;
     }
 }
+/*
+Given the head of a sorted linked list, delete all duplicates such that
+each element appears only once. Return the linked list sorted as well.
+ */
