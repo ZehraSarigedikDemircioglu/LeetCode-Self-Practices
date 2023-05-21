ddf1201f7f2c77d6fae0e33f1992daa8a11476d1
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-20 22:47:24 -0400

    leetcode self practices added

--- src/easy/BinaryTreeInorderTraversal/Solution.java
+++ src/easy/BinaryTreeInorderTraversal/Solution.java
@@ -1,6 +1,22 @@
 package easy.BinaryTreeInorderTraversal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> list = new ArrayList<>();
+
+        if(root == null){
+            return list;
+        }
+
+        list.addAll(inorderTraversal(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversal(root.right));
+        return list;
+    }
 }
 /*
 Given the root of a binary tree, return the inorder traversal of its nodes' values.
