cc01b4ff37aef7bd3864e01008b4e3d804123c0a
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-29 23:12:54 -0400

    leetcode self practices added

---
+++ src/easy/SameTree/Solution.java
@@ -0,0 +1,15 @@
+package easy.SameTree;
+
+public class Solution {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return true;
+    }
+}
+
+/*
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+ */
+
