4aef31aebe308b5a09955256ca9fe25f39487825
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-08-03 17:53:23 -0400

    added practice

---
+++ src/zehra/ReverseString.java
@@ -0,0 +1,19 @@
+package zehra;
+
+public class ReverseString {
+
+    public static char[] reverseString(char[] s) {
+        int right = s.length - 1;
+        int left = 0;
+
+        while (left < right) {
+            char c = s[right];
+            s[right] = s[left];
+            s[left] = c;
+
+            left++;
+            right--;
+        }
+        return s;
+    }
+}
