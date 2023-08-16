27794a889dcb14b245536f88df979190c3a91832
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-08-16 16:54:49 -0400

    added practice

--- src/zehra/TwoSum.java
+++ src/zehra/TwoSum.java
@@ -1,4 +1,25 @@
 package zehra;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+    public static void main(String[] args) {
+        int[] array = new int[]{16, 3, 11, 5, 15};
+        System.out.println(Arrays.toString(twoSum(array, 8)));//[3, 1]
+    }
+
+    public static int[] twoSum(int[] array, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            int expected = target - array[i];
+            if(map.containsKey(expected)){
+                return new int[]{i, map.get(expected)};
+            }else{
+                map.put(array[i], i);
+            }
+        }
+        return new int[]{};
+    }
 }
