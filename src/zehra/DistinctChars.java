9e079e093a180b9786a323361a4f6218947e5ffc
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-08-17 15:07:13 -0400

    added practice

---
+++ src/zehra/DistinctChars.java
@@ -0,0 +1,15 @@
+package zehra;
+
+public class DistinctChars {
+    /*
+    //    Sample Input:
+//            1. ["aaryanna", "aayanna", "airianna", "alassandra", "allanna", "allannah", "allessandra", "allianna",
+//            "allyanna", "anastaisa", "anastashia", "anastasia", "annabella", "annabelle", "annebelle"]
+//    Sample Output
+//    aaryanna - 4
+//    aayanna - 3
+//    Explaination:-
+//    aaryanna has four distinct characters: 'a', 'n', 'r', and 'y'.
+//    aayanna has three distinct characters: 'a', 'n', and 'y'.
+     */
+}
